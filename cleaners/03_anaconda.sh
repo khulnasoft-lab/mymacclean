5ca777344d7dddf05dea1916191007b84e467b8a
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-03-30 19:09:42 +0600

    Create 03_anaconda.sh
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

---
+++ cleaners/03_anaconda.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Check if Anaconda is installed and upgrade packages
+if hash conda 2>/dev/null; then
+
+  echo "Updating Anaconda packages"
+  echo "=========================="
+  conda update --all
+  echo ""
+
+fi
